51c9c97ba40689cd15ec1e880a5461a183bf1c25
Taras Kushnir <taras.kushnir@eleks.com>
2011-11-04 12:16:50 +0200

    Solved some exercises from ocaml-book (list, tail recursion)

--- ocaml/chapter4.ml
+++ ocaml/chapter4.ml
@@ -61,3 +61,56 @@ let map f list = reverse [] (reverse_map f [] list);;
 let x = map f l;;
 
 (* ---------------------------------------- *)
+
+let nth i (x, y, z) =
+  match i with
+    1 -> x
+  | 2 -> y
+  | 3 -> z
+  | _ -> raise (Invalid_argument "nth");;
+
+(* ---------------------------------------- *)
+
+let db = ["John", "x3456", 50.1;
+ "Jane", "x1234", 107.3;
+ "Joan", "unlisted", 12.7];;
+
+
+let find_salary str =
+  let rec find = function
+      (name, _, salary) :: rest when name = str -> salary
+    | _ :: rest -> find rest
+    | [] -> raise (Invalid_argument "no such name")
+  in
+  find db;;
+
+find_salary "Joan";;
+
+
+let select f =
+  let rec get_all buffer = function
+      (name, stuff, salary) :: rest when f (name, stuff, salary) ->
+        get_all ((name, stuff, salary) :: buffer) rest
+    | (_,_,_) :: rest -> get_all buffer rest
+    | [] -> buffer
+  in
+  reverse [] (get_all [] db);;
+
+let select_func = fun (_, _, salary) -> salary < 100.0;;
+
+select select_func;;
+
+(* ---------------------------------------- *)
+
+let rec append_old l1 l2 =
+  match l1 with
+    curr :: rest -> curr :: append_old rest l2
+  | [] -> l2;;
+
+let append l1 l2 =
+  let rec inner_append l2 = function
+      curr :: rest -> inner_append (curr :: l2) rest
+    | [] -> l2
+  in inner_append l2 (List.rev l1);;
+
+let result = append [1;2;3] [4;5;6];;
