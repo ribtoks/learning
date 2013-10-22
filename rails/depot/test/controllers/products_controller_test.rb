dfd0c882f2389c71d7a5e874c28007d446f9516a
Taras Kushnir <kushnirTV@gmail.com>
2013-10-22 13:08:07 +0300

    Added functional tests

--- rails/depot/test/controllers/products_controller_test.rb
+++ rails/depot/test/controllers/products_controller_test.rb
@@ -16,6 +16,11 @@ class ProductsControllerTest < ActionController::TestCase
     get :index
     assert_response :success
     assert_not_nil assigns(:products)
+
+    assert_select 'h1', 'Listing products'
+    assert_select 'table'
+    assert_select 'table tr', :minimum => 3
+    assert_select 'a', 'New product'
   end
 
   test "should get new" do
