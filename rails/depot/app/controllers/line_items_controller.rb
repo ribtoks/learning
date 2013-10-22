434b410c90ce12638b43f37b7981ccd8fb76c174
Taras Kushnir <kushnirTV@gmail.com>
2013-10-22 16:02:22 +0300

    Cart counter stuff

--- rails/depot/app/controllers/line_items_controller.rb
+++ rails/depot/app/controllers/line_items_controller.rb
@@ -30,6 +30,8 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
+        session[:index_counter] = 0
+        
         format.html { redirect_to @line_item.cart, notice: 'Line item was successfully created.' }
         format.json { render action: 'show', status: :created, location: @line_item }
       else
--- rails/depot/app/controllers/store_controller.rb
+++ rails/depot/app/controllers/store_controller.rb
@@ -1,5 +1,19 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
+
+    @counter = increment_counter
+
+    @display_counter = @counter > 5
+  end
+
+  private
+
+  def increment_counter
+    counter = session[:index_counter] || -1
+    counter += 1
+    
+    session[:index_counter] = counter
+    counter
   end
 end
