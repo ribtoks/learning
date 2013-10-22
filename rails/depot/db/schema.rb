26fbe48af2a22c86ee311477c7507adc5b03f488
Taras Kushnir <kushnirTV@gmail.com>
2013-10-22 13:55:03 +0300

    Added basic cart code

--- rails/depot/db/schema.rb
+++ rails/depot/db/schema.rb
@@ -11,7 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131021122850) do
+ActiveRecord::Schema.define(version: 20131022103333) do
+
+  create_table "carts", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "line_items", force: true do |t|
+    t.integer  "product_id"
+    t.integer  "cart_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "products", force: true do |t|
     t.string   "title"
