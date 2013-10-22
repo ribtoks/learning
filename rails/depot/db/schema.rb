2cf08c4e78a5c79285e09e23578a29cf229f4846
Taras Kushnir <kushnirTV@gmail.com>
2013-10-22 16:20:33 +0300

    Added migration to combine different line items in cart

--- rails/depot/db/schema.rb
+++ rails/depot/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131022130326) do
+ActiveRecord::Schema.define(version: 20131022131641) do
 
   create_table "carts", force: true do |t|
     t.datetime "created_at"
