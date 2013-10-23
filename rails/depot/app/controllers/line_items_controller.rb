a3c9f9fa0c4fab07f08e50bc3800afa225d99b46
Taras Kushnir <kushnirTV@gmail.com>
2013-10-23 18:21:57 +0300

    Ajax-based cart on sidebar

--- rails/depot/app/controllers/line_items_controller.rb
+++ rails/depot/app/controllers/line_items_controller.rb
@@ -34,7 +34,8 @@ class LineItemsController < ApplicationController
       if @line_item.save
         session[:index_counter] = 0
         
-        format.html { redirect_to @line_item.cart }
+        format.html { redirect_to store_url }
+        format.js
         format.json { render action: 'show', status: :created, location: @line_item }
       else
         format.html { render action: 'new' }
