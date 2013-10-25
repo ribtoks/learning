2ce564536c12f06cefee507e772ac749fe8e2e6c
Taras Kushnir <kushnirTV@gmail.com>
2013-10-25 13:58:40 +0300

    Added an Atom feed for bought products

--- rails/depot/app/controllers/products_controller.rb
+++ rails/depot/app/controllers/products_controller.rb
@@ -61,6 +61,14 @@ class ProductsController < ApplicationController
     end
   end
 
+  def who_bought
+    @product = Product.find(params[:id])
+
+    respond_to do |format|
+      format.atom
+    end
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_product
