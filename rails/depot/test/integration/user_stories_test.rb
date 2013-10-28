978fd67149315edcf90b23f416fd6def5a8ca3ef
Taras Kushnir <kushnirTV@gmail.com>
2013-10-28 13:29:28 +0200

    Cosmetic refactoring

--- rails/depot/test/integration/user_stories_test.rb
+++ rails/depot/test/integration/user_stories_test.rb
@@ -3,17 +3,19 @@ require 'test_helper'
 class UserStoriesTest < ActionDispatch::IntegrationTest
   fixtures :products
 
+  def setup
+    LineItem.delete_all
+    Order.delete_all
+    @ruby_book = products(:ruby)
+  end
+
   # A user goes to the index page. They select a product, adding it to their
   # cart, and check out, filling in their details on the checkout form. When
   # they submit, an order is created containing their information, along with a
   # single line item corresponding to the product they added to their cart.
 
   test "buying a product" do
-
-    LineItem.delete_all
-    Order.delete_all
-    ruby_book = products(:ruby)
-
+    
     customer_name = "Dave Thomas"
     customer_address = "123 The Street"
     customer_email = "dave@example.com"
@@ -26,12 +28,12 @@ class UserStoriesTest < ActionDispatch::IntegrationTest
 
     # selects a product, adds to cart
 
-    xml_http_request :post, '/line_items', product_id: ruby_book.id
+    xml_http_request :post, '/line_items', product_id: @ruby_book.id
     assert_response :success
 
     cart = Cart.find(session[:cart_id])
     assert_equal 1, cart.line_items.size
-    assert_equal ruby_book, cart.line_items.first.product
+    assert_equal @ruby_book, cart.line_items.first.product
 
     # checkout
 
@@ -66,7 +68,7 @@ class UserStoriesTest < ActionDispatch::IntegrationTest
 
     assert_equal 1, order.line_items.size
     line_item = order.line_items[0]
-    assert_equal ruby_book, line_item.product
+    assert_equal @ruby_book, line_item.product
 
     # check email
 
@@ -74,6 +76,5 @@ class UserStoriesTest < ActionDispatch::IntegrationTest
     assert_equal [customer_email], mail.to
     assert_equal 'Name Surname <depot@example.com>', mail[:from].value
     assert_equal 'Order Confirmation', mail.subject    
-  end
-  
+  end  
 end
