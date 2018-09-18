9351ca27beee1a96cf009e8c1d67326b49cfa1db
Taras Kushnir <takushni@microsoft.com>
2018-09-18 10:35:30 +0300

    Replace array to vector

--- algorithms/sorting/inversions/count_inversions.cpp
+++ algorithms/sorting/inversions/count_inversions.cpp
@@ -1,85 +1,60 @@
 #include <iostream>
+#include <vector>
 
-int merge_with_inversions(int *arr, int left, int split, int right);
+int merge_with_inversions(std::vector<int> &arr, int left, int split, int right);
 
-int mergesort_with_inversions(int *arr, int left, int right) {
-   int inversions = 0;
+int mergesort_with_inversions(std::vector<int> &arr, int left, int right) {
+    int inversions = 0;
 
-   if (left < right) {
-      int mid = left + (right - left) / 2;
+    if (left < right) {
+        int mid = left + (right - left) / 2;
 
-      inversions += mergesort_with_inversions(arr, left, mid);
-      inversions += mergesort_with_inversions(arr, mid + 1, right);
+        inversions += mergesort_with_inversions(arr, left, mid);
+        inversions += mergesort_with_inversions(arr, mid + 1, right);
 
-      inversions += merge_with_inversions(arr, left, mid, right);
-   }
+        inversions += merge_with_inversions(arr, left, mid, right);
+    }
 
-   return inversions;
+    return inversions;
 }
 
-int merge_with_inversions(int *arr, int left, int split, int right) {
-   int *temp = new int[right - left + 1];
-   int p1 = left;
-   int p2 = split + 1;
-   int i = 0;
+// a[i] and a[j] form an inversion if a[i] > a[j] and i < j 
+int merge_with_inversions(std::vector<int> &arr, int left, int split, int right) {
+    std::vector<int> temp(right - left + 1);
+    int p1 = left;
+    int p2 = split + 1;
+    int i = 0;
 
-   int inversions = 0;
+    int inversions = 0;
 
-   while ((p1 <= split) && (p2 <= right)) {
-      if (arr[p1] <= arr[p2]) {
-         temp[i++] = arr[p1++];
-      } else {
-         temp[i++] = arr[p2++];
-         // all items in left subarray form an inversion
-         // for this item which is less that them
-         inversions += split - p1 + 1;
-      }
-   }
+    while ((p1 <= split) && (p2 <= right)) {
+        if (arr[p1] <= arr[p2]) {
+            temp[i++] = arr[p1++];
+        } else {
+            temp[i++] = arr[p2++];
+            // all items in left subarray form an inversion
+            // for this item which is less that them
+            inversions += split - p1 + 1;
+        }
+    }
 
-   while (p1 <= split) {
-      temp[i++] = arr[p1++];
-   }
+    while (p1 <= split) { temp[i++] = arr[p1++]; }
+    while (p2 <= right) { temp[i++] = arr[p2++]; }
 
-   while (p2 <= right) {
-      temp[i++] = arr[p2++];
-   }
+    for (i = left; i <= right; ++i) {
+        arr[i] = temp[i - left];
+    }
 
-   for (i = left; i <= right; ++i) {
-      arr[i] = temp[i - left];
-   }
-
-   delete[] temp;
-
-   return inversions;
+    return inversions;
 }
 
 int main () {
-   int n;
-   std::cin >> n;
-
-   int *arr = new int[n];
-
-   int m = n, a, i = 0;
-   while (m--) {
-      std::cin >> a;
-      arr[i++] = a;
-   }
-
-   int inversions = mergesort_with_inversions(arr, 0, n - 1);
-
-   if (n > 0) {
-      std::cout << arr[0];
-   }
-
-   for (int i = 1; i < n; ++i) {
-      std::cout << " " << arr[i];
-   }
-
-   std::cout << std::endl;
-
-   std::cout << inversions << std::endl;
-
-   delete[] arr;
-
-   return 0;
+    std::vector<int> arr {1, 2, 5, 4, 3};
+    
+    int inversions = mergesort_with_inversions(arr, 0, arr.size() - 1);
+    
+    for (auto &v: arr) { std::cout << v << " "; }
+    
+    std::cout << std::endl << "inversions: " << inversions << std::endl;
+    return 0;
 }
