ea04697d2d39a477d4e54b81f2b668ea3c7fdcaa
Taras Kushnir <kushnirTV@gmail.com>
2019-02-04 12:55:40 +0200

    Cosmetic cleanup

--- ml/deeplearning.ai/start.sh
+++ ml/deeplearning.ai/start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# to build it, use command:
 # docker build -t tklearnml .
 
-docker run -p 8888:8888 -p 6006:6006 -v $(pwd)/notebooks:/notebooks tklearnml
+docker run -e ML_USER=ktar -p 8888:8888 -p 6006:6006 -v $(pwd)/notebooks:/notebooks tklearnml
