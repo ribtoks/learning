40d784f967400f6b6666688dcf7eb30b1ed02e0d
Taras Kushnir <505555+ribtoks@users.noreply.github.com>
2019-05-09 09:18:12 -0700

    Update start.sh

--- ml/docker/start.sh
+++ ml/docker/start.sh
@@ -3,4 +3,6 @@
 # to build it, use command:
 # docker build -t tklearnml .
 
+# after run, open http://localhost:8888/lab?
+
 docker run -e ML_USER=ktar -p 8888:8888 -p 6006:6006 -v $(pwd)/notebooks:/notebooks tklearnml
